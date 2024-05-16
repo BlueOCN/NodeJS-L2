589f1f4c45790f67ddf92e281c415bdc1a8a1042
BlueOCN <robgonzalezmu@gmail.com>
2024-05-16 12:16:12 -0600

    img Update

--- red-bicicletas/models/usuario.js
+++ red-bicicletas/models/usuario.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 const Reserva = require('./reserva');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
 
 var usuarioSchema = new Schema({
     nombre: String
@@ -14,9 +16,6 @@ var usuarioSchema = new Schema({
 
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb){
     var reserva = new Reserva({usuario: this._id, bicicleta: biciId, desde: desde, hasta: hasta});
-    // console.log(reserva);
-    // reserva.save(cb);
-
     reserva.save()
         .then(function(result){
             cb()
